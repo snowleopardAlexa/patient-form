1290fba36fde636cc6bbe5013d98bb79aa73ffca
snowleopardAlexa <alexaslomski@gmail.com>
2021-05-04 23:27:11 -0400

    add state, handle submit

--- patient-form/src/PatientForm.js
+++ patient-form/src/PatientForm.js
@@ -1,14 +1,26 @@
 import React, {useState} from 'react';
 
 function PatientForm() {
+
+const [name, setName] = useState("");
+
+const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`Submitting Name ${name}`)
+}
+
     return (
-      <>
+      <form onSubmit={handleSubmit}>
         <label>
-            Name:
-            <input type="text" />
+            First Name:
+            <input 
+             type="text"
+             value={name}
+             onChange={e => setName(e.target.value)}
+            />
         </label>
         <input type="submit" value="Sumbit" />
-      </>
+      </form>
     );
 }
 
