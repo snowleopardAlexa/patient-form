309f68c1ded0d4610e7da4686a6ddf46e2931b94
snowleopardAlexa <alexaslomski@gmail.com>
2021-05-05 22:03:06 -0400

    add form with bootstrap

--- patient-form/src/PatientForm.js
+++ patient-form/src/PatientForm.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import { useInput } from './hooks/useInput';
+import { Form, Button } from 'react-bootstrap';
 
 function PatientForm() {
 
@@ -15,23 +16,31 @@ const handleSubmit = (e) => {
 }
 
     return (
-      <form onSubmit={handleSubmit}>
-        <label>
-            First Name:
-            <input 
-             type="text"
-             {...bindFirstName}
-            />
-        </label>
-        <label>
-            Last Name:
-            <input 
-             type="text"
-             {...bindLasttName}
-            />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+        <>
+        <h2 className="mt-4">Patient Form</h2>
+        <Form className="mx-auto d-block mt-5" onSubmit={handleSubmit} style={{width: '60%'}}>
+        <Form.Group>
+          <Form.Label className="mb-4">First Name</Form.Label>
+          <Form.Control 
+           type="text" 
+           {...bindFirstName}
+           />
+        </Form.Group>
+      
+        <Form.Group>
+          <Form.Label className=" mt-4 mb-4">Last Name</Form.Label>
+          <Form.Control 
+            type="text" 
+            {...bindLasttName}
+          />
+        </Form.Group>
+       
+      
+        <Button className="mt-4" variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+      </>
     );
 }
 
